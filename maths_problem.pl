e220b2d4307af8d01c52084918dce00b91d327e7
Dave O'Brien <github@diaspoir.net>
2016-03-25 22:08:13 +0700

    Finished solving the problem: brute force method successful.

--- maths_problem.pl
+++ maths_problem.pl
@@ -17,3 +17,75 @@ use warnings;
 # e.g. AB = 21, if A=2, B=1
 #
 # Approach: find all values of Where B - D + H = P mod 10, and F is not B, D, or H
+
+my @numbers; # array to hold the numbers
+#my @stack; # array holding a partial stack, i.e. B, D, F, H, P
+
+my $range = "1..9";
+# brute force it
+foreach my $a (1..9) {
+	foreach my $b (1..9) {
+		foreach my $c (1..9) {
+			foreach my $d (1..9) {
+				if ( numbers_ok($a, $b, $c, $d)) {
+					my $ab = (10 * $a + $b);
+					my $cd = (10 * $c + $d);
+					my $ef = ( $ab - $cd);
+					my $e = int($ef/10);
+					my $f = substr(sprintf("%00d", $ef),-1,1);
+					#print "($a, $b), ($c, $d): $ab - $cd = $ef ($e, $f)\n";
+					if (numbers_ok($a, $b, $c, $d, $e, $f)) {
+						foreach my $g (1..9) {
+							foreach my $h (1..9) {
+								if (numbers_ok($a, $b, $c, $d, $e, $f, $g, $h)) {
+									my $gh = (10 * $g + $h);
+									my $ppp = $ef + $gh;
+									my $ppp_char = sprintf("%03d", $ppp);
+									my $p1 = substr($ppp_char, 0, 1);
+									my $p2 = substr($ppp_char, 1, 1);
+									my $p3 = substr($ppp_char, 2, 1);
+									if ( ($p1 eq $p2) and ($p2 eq $p3)) {
+										print "($a, $b), ($c, $d): $ab - $cd = $ef ($e, $f), ($g, $h), $gh = $ppp ($ppp_char: $p1, $p2, $p3)\n";
+									
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+sub max {
+	# sub to return the maximum value form a list
+	my $max = shift;
+	while (my $num = shift) {
+		if ($num>$max) { $max = $num;}
+	}
+	return $max;
+}
+sub numbers_ok {
+	#receives a list of numbers and returns 1 if no duplicates, 0 otherwise
+	#my @list = $_;
+	my $result = 1;
+	my %count;
+	my $debug = 0;
+	foreach my $element (@_) {
+		$count{$element}++;
+		print "$element ($count{$element}) " if $debug;
+		if ($count{$element}>1) {
+			$result = 0;
+		}
+	}
+	print "\n" if $debug;
+	return $result;
+}
+#sub check_legal_solution {
+#	# sub to check a solution is legal given $p, @numbers
+#	my $p = shift;
+#	my @numbers = $_;
+#	my $ef = 0;
+#	return 0;
+#}
