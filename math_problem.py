cf0c015231b9288869bfb095dbfe377ef964b79f
Dave O'Brien <github@diaspoir.net>
2016-03-29 22:14:18 +0700

    python version running, but returning different results.

--- math_problem.py
+++ math_problem.py
@@ -9,10 +9,16 @@ def max(list):
 	return max
 	
 # step through a list and return 1 if all numbers are unique
-def numbers_ok(list):
+def numbers_ok(list = []):
+	#print list
 	result = 1
+	count = {}
 	for num in list:
-		count[num]++
+		if num not in count:
+			count[num] = 1
+		else:
+			count[num] += 1
+		#print num, count[num]
 		if count[num]>1:
 			result = 0
 	return result
@@ -23,5 +29,18 @@ for a in range(1,9):
 	for b in range(1,9):
 		for c in range(1,9):
 			for d in range(1,9):
-				cd = (10*a+b) - (10*c+d)
-
+				#if numbers_ok([a, b, c, d]):
+				ef = (10*a+b) - (10*c+d)
+				# make a character of the answer: three places integer with sign
+				ef_char = '{:+03d}'.format(ef)
+				#print a, b, c, d, ef, ef_char
+				e = int(ef_char[1])
+				f = int(ef_char[2])
+				for g in range(1,9):
+					for h in range(1,9):
+						ppp = ef + (10 * g + h)
+						ppp_char = '{:+04d}'.format(ppp)
+						if numbers_ok([a, b, c, d, e, f, g, h]):
+							#print a, b, c, d, e, f, g, h, ppp, ppp_char
+							if (ppp_char[1] == ppp_char[2] == ppp_char[3]):
+								print '***', a, b, c, d, e, f, g, h, ppp
