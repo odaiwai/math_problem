142f69c1504dbd3a2b3c999fc52411c49f7fa748
Dave O'Brien <odaiwai@gmail.com>
2023-05-20 18:24:33 +0800

    Finally tidied up the python to be more pythonic.

--- math_problem.py
+++ math_problem.py
@@ -1,59 +1,64 @@
 #!/usr/bin/env python3
+"""
+Script to solve a math problem
+dave o'brien 2016/03/24
+problem:
+  A B
+- C D
+-----
+  E F
++ G H
+-----
+P P P
 
-# Script to solve a math problem
-# dave o'brien 2016/03/24
-# problem:
-#   A B
-# - C D
-# -----
-#   E F
-# + G H
-# -----
-# P P P
-# 
-# Where ABCDEFGH, P are all [1-9], and unique
-# e.g. AB = 21, if A=2, B=1
+Where ABCDEFGH, P are all [1-9], and unique
+e.g. AB = 21, if A=2, B=1
+"""
+from itertools import permutations
 
-# subroutines come first in Python
-# Step through a list, and return the maximum element
-def max(numbers = []):
-    max = numbers.pop(0) # Take the first element
+
+def max(numbers: list) -> int:
+    """
+    Step through a list, and return the maximum element
+    """
+    max = numbers.pop(0)  # Take the first element
     for num in numbers:
-    	if num>max:
-            max=num # if the current number is > max, it becomes max
-    return max
-    
-# step through a list and return 1 if all numbers are unique, 0 otherwise
-def numbers_ok(numbers = []):
-    result = 1
-    count = {} # this defines an empty dictionary
-    for num in numbers:
-    	if num not in count:
-            count[num] = 1 # if we haven't seen this number before, make a new dict. entry
-    	else:
-    	    count[num] += 1 # otherwise increment the number of times we've seen this number
-    	#print(num, count[num])
-    	if count[num]>1:
-    	    result = 0 # if we've seen any number more than one, return 0
-    return result
-    
-# Script to solve the math problem with a brute force method.
+        if num > max:
+            max = num       # if the current number is > max, it becomes max
+    return max(numbers)
 
-# python's range(start, stop, [step]) function is up to, but not including, stop. 
-for a in range(1,10): # all the numbers from one to nine.
-    for b in range(1,10):
-        for c in range(1,10):
-            for d in range(1,10):
-                ef = (10*a+b) - (10*c+d)
-        	# make a character string of the answer: 
-                #  three places integer with sign
-                ef_char = '{:+03d}'.format(ef)
-                e = int(ef_char[1])
-                f = int(ef_char[2])
-                for g in range(1,10):
-                    for h in range(1,10):
-                        ppp = ef + (10 * g + h)
-                        ppp_char = '{:+04d}'.format(ppp)
-                        if numbers_ok([a, b, c, d, e, f, g, h]):
-                            if (ppp_char[1] == ppp_char[2] == ppp_char[3]):
-                                print('solution: ', a, b, c, d, e, f, g, h, ppp)
+
+def numbers_ok(numbers: list) -> bool:
+    """
+    step through a list and return True if all numbers are unique,
+    False otherwise
+    """
+    unique = True
+    count = {}
+    for num in numbers:
+        count[num] = 1 + count.setdefault(num, 0)
+        if count[num] > 1:
+            unique = False  # if we've seen any number more than one, return 0
+    return unique
+
+
+def main():
+    """
+    Main part
+    """
+    for a, b, c, d, g, h in permutations(range(1, 10), 6):
+        ef = (10*a+b) - (10*c+d)
+        # make a character string of the answer:
+        #  three places integer with sign
+        ef_char = '{:+03d}'.format(ef)
+        e = int(ef_char[1])
+        f = int(ef_char[2])
+        ppp = ef + (10 * g + h)
+        ppp_char = '{:+04d}'.format(ppp)
+        if numbers_ok([a, b, c, d, e, f, g, h]):
+            if (ppp_char[1] == ppp_char[2] == ppp_char[3]):
+                print('solution: ', a, b, c, d, e, f, g, h, ppp)
+
+
+if __name__ == '__main__':
+    main()
